6aa512a15f8a6acd8373685bb67086477c180ccb
giordip48 <62465861+giordip48@users.noreply.github.com>
2023-07-18 17:59:48 +0200

    added various types of moving averages

--- MomentumTradingStrategy2.0.py
+++ MomentumTradingStrategy2.0.py
@@ -1,4 +1,7 @@
-# Libraries
+## LIBRARIES
+
+# The library trading_functions is our own coded library which stores useful functions
+# to perform calculations in this backtest environment, we will use it later on.
 
 import pandas as pd
 import numpy
@@ -6,8 +9,10 @@ import wikipedia as wp
 import yfinance as yf
 import matplotlib.pyplot as plt
 import datetime as dt
+import trading_functions
 from datetime import timedelta
-import week_Number 
+
+## GATHERING DATA
 
 # Downloading tickers from Wikipedia
 
@@ -43,6 +48,8 @@ else: df = pd.read_excel('Prova.xlsx')
 
 sp500_price_history = (yf.download('^GSPC', start=start_date, progress=False)['Adj Close']).reset_index()
 
+## PROCESSING DATA
+
 # Converting "Date" Column into a more familiar datatype for both dataframes
 
 df['Date'] = list(map(lambda i: dt.datetime.strptime(str(i)[0:10], "%Y-%m-%d"), df['Date']))
@@ -53,8 +60,31 @@ sp500_price_history['Date'] = list(map(lambda i: dt.datetime.strptime(str(i)[0:1
 df['Week Number'] = list(map(lambda i: i.strftime("%U"), df['Date']))
 sp500_price_history['Week Number'] = list(map(lambda i: i.strftime("%U"), sp500_price_history['Date']))
 
-#il numero settimana è solo dell'anno, deve essere dalla partenza dello script 
+# Using a library we coded ourselves to keep track of weeks that have passed
 
-df = week_Number.week_number_total_n(df)
+df = trading_functions.week_number_total_n(df)
+sp500_price_history = trading_functions.week_number_total_n(sp500_price_history)
 
-print(df)
+# Create Simple Moving Average for S&P500
+
+# n variable represents the period for computing the moving average
+
+periods = [12, 30, 90, 100]
+moving_average_types = ['MA', 'EMA']
+
+for type_moving_average in moving_average_types:
+    for period_MA in periods:
+        sp500_price_history = trading_functions.calculate_moving_average(sp500_price_history,type_moving_average,period_MA)
+
+print(sp500_price_history.tail(3))
+
+#sp500_price_history = trading_functions.calculate_moving_average(sp500_price_history,'EMA', periods[1])
+
+# Function to plot the S&P 500 graph
+
+def plot_sp500_graph(sp500_price_history, ema_name):
+
+    plt.figure(figsize=(20, 10))
+    plt.plot(sp500_price_history['Date'],sp500_price_history[ema_name], color="Blue", label="Moving Average")
+    plt.plot(sp500_price_history['Date'],sp500_price_history['Adj Close'], color='Red', label="S&P500")
+    plt.show()
---
+++ trading_functions.py
@@ -0,0 +1,74 @@
+import pandas as pd
+from datetime import timedelta
+import datetime
+
+def week_number_total_n(df):
+    df['Week Number'] = df['Week Number'].apply(int)
+    df['year'] = list(map(lambda i: (i.year-2021)*52, df['Date']))
+    df['Week Number'] = df['Week Number'] + df['year']
+    df = df.drop(columns='year',axis=1)
+    
+    return df
+
+def calculate_moving_average(df,moving_average_type,period):
+
+    # Creating an empty array to store moving average data
+    
+    moving_average_array = []
+    
+    name = moving_average_type + "_" + str(period)
+
+    # Calculating moving average 
+
+    if moving_average_type == 'MA':
+
+        # Calculating simple moving average 
+    
+        for i in range(len(df)):
+        
+            if i >= period:
+                moving_average = df['Adj Close'][i-period:i].sum() / period
+                moving_average_array.append(moving_average)
+
+            # It's impossible to compute moving average 
+            # with period n for a number of observation inferior to n, 
+            # so we are skipping the first n days
+
+            else: moving_average_array.append(None)
+
+    elif moving_average_type == 'EMA':
+
+        # Calculating exponential moving average 
+
+            # a represents smoothing factor, used to compute EMA. 
+
+            a = 0.75
+
+            # ema_shifted represents ema calculated the day before
+                
+            ema_shifted = 0
+
+            for i in range(len(df)):
+                
+                if i == period:
+                    moving_average = df['Adj Close'][i-period:i].sum() / period
+                    moving_average_array.append(moving_average)
+                    ema_shifted = moving_average
+
+                elif i > period:
+                    CN = df['Adj Close'][i-1]
+                    ema = ((a * CN) + (1 - a) * ema_shifted)
+                    moving_average_array.append(ema)
+                    ema_shifted = ema
+
+            # It's impossible to compute moving average 
+            # with period n for a number of observation inferior to n, 
+            # so we are skipping the first n days
+
+                else: moving_average_array.append(None)
+
+    # Adding moving average to our S&P 500 dataframe
+
+    df[name] = moving_average_array
+
+    return df
