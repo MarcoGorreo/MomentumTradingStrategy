a14dd1c6b0f4475f87510de1aee78cad42b8c613
giordip48 <62465861+giordip48@users.noreply.github.com>
2023-07-20 15:29:57 +0200

    becktest enviroment creation

--- MomentumTradingStrategy2.0.py
+++ MomentumTradingStrategy2.0.py
@@ -30,7 +30,7 @@ start_date = "2021-01-01"
 
 # Creating an empty dataframe to fill with data of each ticker
 
-df = pd.DataFrame()
+stocks_df = pd.DataFrame()
 
 data_download = False
 
@@ -39,10 +39,10 @@ data_download = False
 if data_download == True:
     for t in sp500_tickers:
         try: 
-            df[t] = yf.download(t, start=start_date, progress=False)['Adj Close']
-            print(df[t])
+            stocks_df[t] = yf.download(t, start=start_date, progress=False)['Adj Close']
+            print(stocks_df[t])
         except: pass
-else: df = pd.read_excel('Prova.xlsx')
+else: stocks_df = pd.read_excel('Prova.xlsx')
 
 # Downloading price history of S&P500
 
@@ -52,31 +52,34 @@ sp500_price_history = (yf.download('^GSPC', start=start_date, progress=False)['A
 
 # Converting "Date" Column into a more familiar datatype for both dataframes
 
-df['Date'] = list(map(lambda i: dt.datetime.strptime(str(i)[0:10], "%Y-%m-%d"), df['Date']))
+stocks_df['Date'] = list(map(lambda i: dt.datetime.strptime(str(i)[0:10], "%Y-%m-%d"), stocks_df['Date']))
 sp500_price_history['Date'] = list(map(lambda i: dt.datetime.strptime(str(i)[0:10], "%Y-%m-%d"), sp500_price_history['Date']))
 
-# Counting the number of weeks that have passed since the start of the dataset will help us later
+# Counting the number of weeks that have passed since the start of the dataset, this will help us later
 
-df['Week Number'] = list(map(lambda i: i.strftime("%U"), df['Date']))
+stocks_df['Week Number'] = list(map(lambda i: i.strftime("%U"), stocks_df['Date']))
 sp500_price_history['Week Number'] = list(map(lambda i: i.strftime("%U"), sp500_price_history['Date']))
 
-# Using a library we coded ourselves to keep track of weeks that have passed
+# Using a function from a library we coded ourselves to keep track of weeks that have passed
 
-df = trading_functions.week_number_total_n(df)
+stocks_df = trading_functions.week_number_total_n(stocks_df)
 sp500_price_history = trading_functions.week_number_total_n(sp500_price_history)
 
 # Create Simple Moving Average for S&P500
-
 # n variable represents the period for computing the moving average
 
 periods = [12, 30, 90, 100]
 moving_average_types = ['MA', 'EMA']
+distinct_moving_average_setting = []
 
 for type_moving_average in moving_average_types:
     for period_MA in periods:
+
         sp500_price_history = trading_functions.calculate_moving_average(sp500_price_history,type_moving_average,period_MA)
 
-print(sp500_price_history.tail(3))
+        # Filling an array with all different kind of moving averages 
+
+        distinct_moving_average_setting += [f"{type_moving_average}_{period_MA}"]
 
 #sp500_price_history = trading_functions.calculate_moving_average(sp500_price_history,'EMA', periods[1])
 
@@ -88,3 +91,28 @@ def plot_sp500_graph(sp500_price_history, ema_name):
     plt.plot(sp500_price_history['Date'],sp500_price_history[ema_name], color="Blue", label="Moving Average")
     plt.plot(sp500_price_history['Date'],sp500_price_history['Adj Close'], color='Red', label="S&P500")
     plt.show()
+
+# We are recalling a function from our personal libary to determinate if the actual momentum of S&P500 is currently long
+# It will check for each data point if the actual price is long 
+
+sp500_price_history = trading_functions.determinate_momentum(sp500_price_history, "MA_30")
+
+# It will give an error while checking the data with moving averages with different periods:
+# MA with 90 days period will require more days to compute than a 30 days period moving average 
+
+sp500_price_history = sp500_price_history[sp500_price_history['Momentum'] != "Error"]
+
+sp500_price_history = sp500_price_history.reset_index(drop=True)
+
+## CREATING BACKTEST ENVIRONMENT
+
+weights_if_long = [0.80,0.20]
+weights_if_short = [0.20,0.80]
+n_assets = 2
+
+def backtest(sp500_price_history, stocks_df, weights_if_long, weights_if_short, n_assets):
+
+    for i in range(max(sp500_price_history['Week Number'])):
+        print(sp500_price_history[sp500_price_history['Week Number'] == i])
+
+backtest(sp500_price_history, stocks_df, weights_if_long, weights_if_short, n_assets)
--- trading_functions.py
+++ trading_functions.py
@@ -6,6 +6,7 @@ def week_number_total_n(df):
     df['Week Number'] = df['Week Number'].apply(int)
     df['year'] = list(map(lambda i: (i.year-2021)*52, df['Date']))
     df['Week Number'] = df['Week Number'] + df['year']
+    df['Week Number'] = df['Week Number'].apply(int)
     df = df.drop(columns='year',axis=1)
     
     return df
@@ -72,3 +73,20 @@ def calculate_moving_average(df,moving_average_type,period):
     df[name] = moving_average_array
 
     return df
+
+def determinate_momentum(sp_500_data, ema):
+
+    momentum = []
+
+    for i in range(len(sp_500_data)):
+        if sp_500_data['Adj Close'][i] >= sp_500_data[ema][i]:
+            momentum.append("Long")
+        elif sp_500_data['Adj Close'][i] <= sp_500_data[ema][i]:
+            momentum.append("Short")
+        else: momentum.append('Error')
+
+    sp_500_data['Momentum'] = momentum
+    
+    sp_500_data = sp_500_data[["Date", "Adj Close", ema,"Momentum", "Week Number"]]
+
+    return sp_500_data
