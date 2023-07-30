4799958f9bfdae1b412be702c64ce838588d6852
giordip48 <62465861+giordip48@users.noreply.github.com>
2023-07-30 11:06:22 +0200

    dalete io

--- io.py
+++
@@ -1,7 +0,0 @@
-possible_allocation_combinations_long = list(zip([round((i*0.10),2) for i in range(1,10)],[round((1 - i*0.10),2) for i in range(1,10)]))
-possible_allocation_combinations_short = list(zip([round((i*0.10),2) for i in range(10,1,-1)],[round((1 - (i*0.10)),2) for i in range(10,1,-1)]))
-test = [(i[1],i[0]) for i in possible_allocation_combinations_long]
-
-print(possible_allocation_combinations_long)
-print(possible_allocation_combinations_short)
-print(test)
